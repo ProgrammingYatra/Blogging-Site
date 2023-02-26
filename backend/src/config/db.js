bc67e59a95c21939d2233bde36e481b63783a4a2
Rupam Gupta <guptarupam.1020@gmail.com>
2023-02-26 23:28:26 +0530

    Database File Done

---
+++ backend/src/config/db.js
@@ -0,0 +1,10 @@
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.USERNAME,
+  password:process.env.PASSWORD,
+  database: process.env.DATABASE,
+});
+console.log("MySQL is Connected Successfully");
+module.exports = db;
